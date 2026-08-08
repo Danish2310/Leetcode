6245eb09d84bf6a378f25dc55fddc8b855366383
Danish2310 <155991007+Danish2310@users.noreply.github.com>
2026-08-08 11:08:04 +0530

    Time: 14 ms (92.04%) | Memory: 51.6 MB (19.57%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int count=0;
+        int i=0;
+        int m=g.length;
+        int n=s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        for(int j=0;i<m&&j<n;j++){
+            if(g[i]<=s[j]){
+                count++;
+               
+                // if(i+1<g.length){
+                //     i++; 
+                // }
+                i++;
+            }
+        }
+       
+        return count;
+    }
+} 
+// Set<Integer> set=new HashSet<>();
+        // for(int i=0;i<g.length;i++){
+        //     set.add(g[i]);
+        // }
+        // for(int j =0;j<s.length;j++){
+        //     if(set.contains(s[j])){
+        //         count++;
+        //         set.remove(s[j]);
+        //     }
+        // }
