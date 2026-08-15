ce7eccd0a654058018f9a796aab005fc2e709f23
Danish2310 <155991007+Danish2310@users.noreply.github.com>
2026-08-15 23:20:15 +0530

    Time: 4 ms (11.85%) | Memory: 46.5 MB (42.11%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        if((prices[0]+prices[1])<=money){
+            int remaining_amount=money-(prices[0]+prices[1]);
+            return remaining_amount;
+        }
+        
+            return money;
+        
+
+        
+    }
+}
