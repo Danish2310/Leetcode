f156aa88879ee4f69e81ba80b89d23ba8f7753b6
Danish2310 <155991007+Danish2310@users.noreply.github.com>
2026-08-19 00:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (35.20%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,24 +1,21 @@
 class Solution {
     public char repeatedCharacter(String s) {
         String copy=new String(s);
-        HashMap<Character,Integer> map=new HashMap<>();
+        // HashMap<Character,Integer> map=new HashMap<>();
+        HashSet<Character> set=new HashSet<>();
         for(int i=0;i<s.length();i++){
-            int count=map.getOrDefault(s.charAt(i),0)+1;
-            map.put(s.charAt(i),count);
-            if(map.getOrDefault(copy.charAt(i),0)==2){
-                return copy.charAt(i);
+            // int count=map.getOrDefault(s.charAt(i),0)+1;
+            // map.put(s.charAt(i),count);
+            // if(map.getOrDefault(copy.charAt(i),0)==2){
+            //     return copy.charAt(i);
+            // }
+            if(set.contains(s.charAt(i))){
+                return s.charAt(i);
             }
+            set.add(s.charAt(i));
         }
-        // for(int i=0;i<copy.length();i++){
-        //     if(map.getOrDefault(copy.charAt(i),0)==2){
-        //         return copy.charAt(i);
-        //     }
-        //     // else{
-        //     //     return -1;
-        //     // }
-        // }
 
-        return 'a';
+        return ' ';
         
     }
 }
