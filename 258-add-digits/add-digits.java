c445cafc735d0d1235a2cbac1cf7fc0e00f7ee00
Danish2310 <155991007+Danish2310@users.noreply.github.com>
2026-08-18 23:42:10 +0530

    Time: 1 ms (97.98%) | Memory: 42.4 MB (80.91%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,16 +1,24 @@
 class Solution {
     public int addDigits(int num) {
-        int minsum=Integer.MAX_VALUE;
-        int cursum=0;
-        if(num<10){
-            return num;
+       while(num>=10){
+        int sum=0;
+        while(num>0){
+            sum=sum+num%10;
+            num=num/10;
         }
-        while (minsum >= 10){
-            cursum=(num%10) + (num/10) ;
-            num=cursum;
-            minsum=Math.min(minsum,cursum);
-        }
-        return minsum;
+        num=sum;
+       }
+        return num;
         
     }
 }
+//  int minsum=Integer.MAX_VALUE;
+//         int cursum=0;
+//         if(num<10){
+//             return num;
+//         }
+//         while (minsum >= 10){
+//             cursum=(num%10) + (num/10) ;
+//             num=cursum;
+//             minsum=Math.min(minsum,cursum);
+//         }
