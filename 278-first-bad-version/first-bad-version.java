54560aba92eb8e899d7bf342c9106b900fc7dc77
Danish2310 <155991007+Danish2310@users.noreply.github.com>
2026-08-30 23:58:36 +0530

    Time: 14 ms (50.99%) | Memory: 41.6 MB (98.44%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -5,18 +5,19 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int l=1;
         int r=n;
-        int firstpos=Integer.MAX_VALUE;
+        // int firstpos=Integer.MAX_VALUE;
         while(l<=r){
             
             int mid=l+(r-l)/2;
             if(isBadVersion(mid)){
                 r=mid-1;
-                firstpos=Math.min(firstpos,mid);
+                // firstpos=Math.min(firstpos,mid);
             }
             else{
                 l=mid+1;
             }
         }
-        return firstpos;
+        // return firstpos;
+        return l;
     }
 }
